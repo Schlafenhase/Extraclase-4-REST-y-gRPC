e87e268fdca60657b88ec422a01dd999ec3b39e2
Jesus Sandoval <sandoval14yamir@hotmail.com>
2021-05-01 16:52:47 -0600

    Push inicial del REST

---
+++ REST/ExtraClase4/Controller/ApplicationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExtraClase4.Controller
+{
+    [Route("Api/Application")]//Ruta principal de la aplicacion, controller se lee como el nombre del documento, en este caso Application.
+    public class ApplicationController : ControllerBase
+    {
+        [Route("file")]
+        [HttpGet]
+        public string GetText()
+        {
+            return System.IO.File.ReadAllText("C:/Users/Jesus/Documents/Semestre 1-2021/SOAD/Extraclase-4-REST-y-gRPC/REST/texto.txt");
+        }
+    }
+}
